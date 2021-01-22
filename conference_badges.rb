6c86770db4516a5ca2ba3bee3b6bb5c1d6badb35
kalille Overton <>
2021-01-22 14:16:12 -0500

    commit tree b92b39b6d6664a31493eeb4ff524c5ff88b6e111

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,31 @@
 # Write your code here.
+require "pry"
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+
+
+    def batch_badge_creator(array)
+        array.map {|badge| badge_maker(badge)}
+    end
+         
+    def assign_rooms(array)
+        array.each_with_index.map {|name, num| "Hello, #{name}! You'll be assigned to room #{num+1}!"}
+    end
+    
+
+    def printer(array)
+        batch_badge_creator(array).each do |badge|
+        
+        puts badge
+        end
+        assign_rooms(array).each do |room|
+            puts room
+        end
+        
+    end
+
+         
+
+
